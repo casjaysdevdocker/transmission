56d770c667df4d23c2d764501a5768c9afd3ebd3
casjay <git-admin@casjaysdev.com>
2022-09-26 13:31:27 -0400

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM casjaysdevdocker/alpine:latest as build
+
+ARG LICENSE=WTFPL \
+  IMAGE_NAME=transmission \
+  TIMEZONE=America/New_York \
+  PORT=
+
+ENV SHELL=/bin/bash \
+  TERM=xterm-256color \
+  HOSTNAME=${HOSTNAME:-casjaysdev-$IMAGE_NAME} \
+  TZ=$TIMEZONE
+
+RUN mkdir -p /bin/ /config/ /data/ && \
+  rm -Rf /bin/.gitkeep /config/.gitkeep /data/.gitkeep && \
+  apk update -U --no-cache
+
+COPY ./bin/. /usr/local/bin/
+COPY ./config/. /config/
+COPY ./data/. /data/
+
+FROM scratch
+ARG BUILD_DATE="$(date +'%Y-%m-%d %H:%M')"
+
+LABEL org.label-schema.name="transmission" \
+  org.label-schema.description="Containerized version of transmission" \
+  org.label-schema.url="https://hub.docker.com/r/casjaysdevdocker/transmission" \
+  org.label-schema.vcs-url="https://github.com/casjaysdevdocker/transmission" \
+  org.label-schema.build-date=$BUILD_DATE \
+  org.label-schema.version=$BUILD_DATE \
+  org.label-schema.vcs-ref=$BUILD_DATE \
+  org.label-schema.license="$LICENSE" \
+  org.label-schema.vcs-type="Git" \
+  org.label-schema.schema-version="latest" \
+  org.label-schema.vendor="CasjaysDev" \
+  maintainer="CasjaysDev <docker-admin@casjaysdev.com>"
+
+ENV SHELL="/bin/bash" \
+  TERM="xterm-256color" \
+  HOSTNAME="casjaysdev-transmission" \
+  TZ="${TZ:-America/New_York}"
+
+WORKDIR /root
+
+VOLUME ["/root","/config","/data"]
+
+EXPOSE $PORT
+
+COPY --from=build /. /
+
+ENTRYPOINT [ "tini", "--" ]
+HEALTHCHECK CMD [ "/usr/local/bin/entrypoint-transmission.sh", "healthcheck" ]
+CMD [ "/usr/local/bin/entrypoint-transmission.sh" ]
+
---
+++ bin/entrypoint-transmission.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202209261331-git
+# @@Author           :  Jason Hempstead
+# @@Contact          :  jason@casjaysdev.com
+# @@License          :  WTFPL
+# @@ReadME           :  entrypoint-transmission.sh --help
+# @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @@Created          :  Monday, Sep 26, 2022 13:31 EDT
+# @@File             :  entrypoint-transmission.sh
+# @@Description      :  
+# @@Changelog        :  New script
+# @@TODO             :  Better documentation
+# @@Other            :  
+# @@Resource         :  
+# @@Terminal App     :  no
+# @@sudo/root        :  no
+# @@Template         :  other/docker-entrypoint
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$0" 2>/dev/null)"
+VERSION="202209261331-git"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
+SCRIPT_SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set bash options
+[ "$1" == "--debug" ] && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"
+[ "$1" == "--raw" ] && export SHOW_RAW="true"
+set -o pipefail
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set functions
+__version() { echo -e ${GREEN:-}"$VERSION"${NC:-}; }
+__find() { ls -A "$*" 2>/dev/null; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# colorization
+[ -n "$SHOW_RAW" ] || printf_color() { echo -e '\t\t'${2:-}"${1:-}${NC}"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_bash() {
+  local cmd="${*:-/bin/bash}"
+  local exitCode=0
+  echo "Executing command: $cmd"
+  $cmd || exitCode=10
+  return ${exitCode:-$?}
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Define default variables
+TZ="${TZ:-America/New_York}"
+HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
+BIN_DIR="${BIN_DIR:-/usr/local/bin}"
+DATA_DIR="${DATA_DIR:-$(__find /data/ 2>/dev/null | grep '^' || false)}"
+CONFIG_DIR="${CONFIG_DIR:-$(__find /config/ 2>/dev/null | grep '^' || false)}"
+CONFIG_COPY="${CONFIG_COPY:-false}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Export variables
+export TZ HOSTNAME
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# import variables from file
+[ -f "/root/env.sh" ] && . "/root/env.sh"
+[ -f "/config/.env.sh" ] && . "/config/.env.sh"
+[ -f "/root/env.sh" ] && [ ! -f "/config/.env.sh" ] && cp -Rf "/root/env.sh" "/config/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set timezone
+[ -n "${TZ}" ] && echo "${TZ}" >/etc/timezone
+[ -f "/usr/share/zoneinfo/${TZ}" ] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set hostname
+if [ -n "${HOSTNAME}" ]; then
+  echo "${HOSTNAME}" >/etc/hostname
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >/etc/hosts
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Delete any gitkeep files
+[ -n "${CONFIG_DIR}" ] && { [ -d "${CONFIG_DIR}" ] && rm -Rf "${CONFIG_DIR}/.gitkeep" || mkdir -p "/config/"; }
+[ -n "${DATA_DIR}" ] && { [ -d "${DATA_DIR}" ] && rm -Rf "${DATA_DIR}/.gitkeep" || mkdir -p "/data/"; }
+[ -n "${BIN_DIR}" ] && { [ -d "${BIN_DIR}" ] && rm -Rf "${BIN_DIR}/.gitkeep" || mkdir -p "/bin/"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy config files to /etc
+if [ -n "${CONFIG_DIR}" ] && [ "${CONFIG_COPY}" = "true" ]; then
+  for config in ${CONFIG_DIR}; do
+    if [ -d "/config/$config" ]; then
+      [ -d "/etc/$config" ] || mkdir -p "/etc/$config"
+      cp -Rf "/config/$config/." "/etc/$config/"
+    elif [ -f "/config/$config" ]; then
+      cp -Rf "/config/$config" "/etc/$config"
+    fi
+  done
+fi
+[ -f "/etc/.env.sh" ] && rm -Rf "/etc/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional commands
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+case "$1" in
+--help) # Help message
+  echo 'Docker container for '$APPNAME''
+  echo "Usage: $APPNAME [healthcheck, bash, command]"
+  echo "Failed command will have exit code 10"
+  echo
+  exitCode=$?
+  ;;
+
+healthcheck) # Docker healthcheck
+  echo "$(uname -s) $(uname -m) is running"
+  echo _other_commands here
+  exitCode=$?
+  ;;
+
+*/bin/sh | */bin/bash | bash | shell | sh) # Launch shell
+  shift 1
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
+  ;;
+
+*) # Execute primary command
+  if [ $# -eq 0 ]; then
+    __exec_bash "/bin/bash"
+  else
+    __exec_bash "/bin/bash"
+  fi
+  exitCode=$?
+  ;;
+esac
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end of entrypoint
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
